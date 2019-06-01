bce974734db6fbe0eb938823351598df0e270660
Cory <Coryf65@gmail.com>
2019-05-31 20:39:45 -0500

    If elif else, all the conditionals ! no other fancy ways

--- Ch2/conditionals_start.py
+++ Ch2/conditionals_start.py
@@ -3,12 +3,25 @@
 #
 
 def main():
-  x, y = 10, 100
+  # declaring many vars inline
+  x, y = 100, 100
   
-  # conditional flow uses if, elif, else  
+  # conditional flow uses if, elif (else if, in C#), else  
+  # can use string interpolation using the keyword f"{var/object}"
+  if (x < y):
+    st = f"{x} is less than {y}"
+  elif(x == y):
+    st = f"{x} is the same as {y}"
+  else:
+    st = f"{x} is greater than {y}"
+  print(st)
 
-  # conditional statements let you use "a if C else b"
+  # like a case statement, or switch statements, NOPE!!! just else if's
   
+  # conditional statements let you use "a if C else b", just a shorthand if else similar to the turinary statement
+  st = "x is less than y" if (x<y) else "x is greater than y"
+  print(st)   
+
 
 if __name__ == "__main__":
   main()
