15449b9f2855c4409235c42f8c3c433e867e0529
Cory <Coryf65@gmail.com>
2019-06-01 14:52:19 -0500

    can use read writes or appending!

--- Ch4/files_start.py
+++ Ch4/files_start.py
@@ -5,23 +5,28 @@
 def main():  
   # Open a file for writing and create it if it doesn't exist
   # goto 'textfile.txt', with read/write and '+' create if not exists
-  f = open("textfile.txt", "w+")
-
-
-
+  # overwrites existing content
+    #f = open("textfile.txt", "w+")
 
   # Open the file for appending text to the end
+    #f = open("textfile.txt", "a")
+
+
+  # now trying with Read access
+  f = open("textfile.txt", "r")
 
 
   # write some lines of data to the file
-  for i in range(10):
-    f.write("This is a line " + str(i) + "\r\n")
+    # for i in range(10):
+    #   f.write("This is a line " + str(i) + "\r\n")
   
   # close the file when done
-  f.close()
+    # f.close()
   
   # Open the file back up and read the contents
-
+  if f.mode == 'r':
+    contents = f.read()
+    print(contents)
     
 if __name__ == "__main__":
   main()
