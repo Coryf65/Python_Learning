f7c3be757b5d8cd34e5d61acf0b4816192fcd7c8
Cory <Coryf65@gmail.com>
2019-06-04 20:05:33 -0500

    using shutil ! copying files

--- Ch4/shell_start.py
+++ Ch4/shell_start.py
@@ -7,16 +7,23 @@ import shutil
 
 
 def main():
+
+  test_file = "textfile.txt"
+
   # make a duplicate of an existing file
   if path.exists("textfile.txt"):
     # get the path to the file in the current directory
+    print("\n")
+    source = path.realpath(test_file)
 
     
     # let's make a backup copy by appending "bak" to the name
-
+    dst = source + ".bak"
     
     # copy over the permissions, modification times, and other info
-
+    shutil.copy(source, dst) # this copies contents
+    # if you want the file metadata
+    shutil.copystat(source, dst)
 
     # rename the original file
 
